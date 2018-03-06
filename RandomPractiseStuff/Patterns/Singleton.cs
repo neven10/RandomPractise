bf203c61ab1fa2bf720a8fe3ed45804a1b11458b
Neven <nevenlalic10@gmail.com>
2018-03-06 16:08:05 +0100

    no message

---
+++ RandomPractiseStuff/Patterns/Singleton.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RandomPractiseStuff
+{
+    public class Singleton
+    {
+
+        private static volatile Singleton _instance;
+        private static object syncLock = new object();
+
+
+
+        public Singleton()
+        {
+
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (_instance != null)
+                {
+                    return _instance;
+                }
+
+                lock (syncLock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new Singleton();
+                    }
+                }
+                return _instance;
+            }
+        }
+        public void Msg(string poruka)
+        {
+            Console.WriteLine(poruka);
+            Console.ReadLine();
+        }
+
+
+
+
+
+
+
+
+    }
+}
---
+++ RandomPractiseStuff/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomPractiseStuff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton.Instance.Msg("!");
+        }
+    }
+}
