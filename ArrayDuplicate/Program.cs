a0d762bf15fd515b69bf91d5d50c78153b45b2b4
Neven <nevenlalic10@gmail.com>
2018-05-14 13:28:00 +0200

    .

---
+++ ArrayDuplicate/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayDuplicate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 5, 5, 55, 565, 4, 849, 8494, 494, 53, 161, 43, 3, 3, };
+            var d = new Dictionary<int, int>();
+            foreach (var i in array)
+            {
+                if (!d.ContainsKey(i))
+                {
+                    d.Add(i, 0);
+                }
+                d[i]++;
+            }
+
+            foreach(var o in d)
+            {
+                Console.WriteLine("{0}, {1}", o.Key, o.Value);
+            }
+            Console.ReadLine();
+
+
+
+        }
+
+        
+    }
+}
